e8dabf67c1dd697981c6e26efc8aa7bb1ba316d5
谷本　祐一 <ytanimot@MBP-15UAC-084.local>
2017-07-10 23:14:32 +0900

    fix

--- interface_sample.go
+++ interface_sample.go
@@ -31,21 +31,21 @@ func (table *Table) addPlayer(num int) {
 	table.Players += num
 }
 
-func doManage(manager TableManager) {
+func DoManage(manager TableManager) {
 	manager.addPlayer(-2)
 	fmt.Printf("Table %v has %v players left.\n", manager.getTableID(), manager.countPlayer())
 }
 
-func newTable(id, players int) *Table {
+func NewTable(id, players int) *Table {
 	return &Table{id, players}
 }
 
-func newDesk(id, players int) *Desk {
-	return &Desk{Table: newTable(id, players)}
+func NewDesk(id, players int) *Desk {
+	return &Desk{Table: NewTable(id, players)}
 }
 
 // Tableを埋め込んだDesk特有の関数
-func (desk Desk) getDeskInfo() {
+func (desk Desk) GetDeskInfo() {
 	fmt.Printf("Desck %v has %v players left.\n", desk.getTableID(), desk.countPlayer())
 }
 
@@ -53,12 +53,12 @@ func main() {
 	tables := make([]Table, 0)
 	// 失敗する
 	// var table Table = &Table{1, 7}
-	// doManage(table)
+	// DoManage(table)
 
 	// 成功する。何故？
-	table1 := newTable(1, 7)
-	table2 := newTable(2, 8)
-	table3 := newTable(3, 9)
+	table1 := NewTable(1, 7)
+	table2 := NewTable(2, 8)
+	table3 := NewTable(3, 9)
 	tables = append(tables, *table1)
 	tables = append(tables, *table2)
 	tables = append(tables, *table3)
@@ -67,19 +67,19 @@ func main() {
 	// おそらく理由はvに対してはtable要素のコピーが走っているから
 	// ポインタを渡せる方法があれば知りたい
 	// for i, v := range tables {
-	//         doManage(&v)
+	//         DoManage(&v)
 	// }
 	for i, _ := range tables {
-		doManage(&tables[i])
+		DoManage(&tables[i])
 	}
 
-	// doManagerが呼び出された後に値が変更されていることを確認
+	// DoManagerが呼び出された後に値が変更されていることを確認
 	for i, v := range tables {
-		fmt.Printf("After doManage, tables[%v].Players: %v\n", i, v.Players)
+		fmt.Printf("After DoManage, tables[%v].Players: %v\n", i, v.Players)
 	}
 
 	// embedができているかの確認
-	desk := newDesk(4, 10)
-	doManage(desk)
-	desk.getDeskInfo()
+	desk := NewDesk(4, 10)
+	DoManage(desk)
+	desk.GetDeskInfo()
 }
